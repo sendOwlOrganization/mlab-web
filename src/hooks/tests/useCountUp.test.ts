37f09ccfa460075a1dfdf4bb14ad53f6ecbf7c4f
Sangbin Lee <leesangbin@outlook.com>
2023-03-25 01:48:55 +0900

    :white_check_mark: useCountUp 테스트 추가

--- src/hooks/tests/useCountUp.test.ts
+++ src/hooks/tests/useCountUp.test.ts
@@ -12,45 +12,42 @@ describe("useCountUp 테스트", () => {
     jest.useRealTimers()
   })
 
-  describe("duration이 1초 일 때", () => {
-    test("목표 수치가 0이면 카운트도 1초후에 0이다", () => {
-      const { result } = renderHook(() => useCountUp(0, 1_000))
+  describe.each([1_000, 5_000])("duration이 %dms 일 때", (givenDuration) => {
+    const seconds = (givenDuration / 1_000).toFixed(2)
+
+    test.each([
+      { given: 0, expected: 0 },
+      { given: 1_234, expected: 1_234 },
+      { given: 50_433, expected: 50_433 },
+      { given: 2_403, expected: 2_403 }
+    ])(`목표 수치가 $given이면 카운트도 ${seconds}초 후에 $expected이다`, ({ given, expected }) => {
+      const { result } = renderHook(() => useCountUp(given, givenDuration))
 
       act(() => {
-        jest.advanceTimersByTime(1_000)
+        jest.advanceTimersByTime(givenDuration)
       })
 
-      expect(result.current).toBe(0)
+      expect(result.current).toBe(expected)
     })
 
-    test("목표 수치가 5,000이면 카운트도 1초후에 1,000이다", () => {
-      const { result } = renderHook(() => useCountUp(5_000, 1_000))
+    test.each([52, 492, 2_033])(`${seconds}초가 되지 않았을 때 카운트가 목표 수치(%d)보다 낮아야한다`, (givenEnd) => {
+      const { result } = renderHook(() => useCountUp(givenEnd, givenDuration))
 
       act(() => {
-        jest.advanceTimersByTime(1_000)
+        jest.advanceTimersByTime(givenDuration / 2)
       })
 
-      expect(result.current).toBe(5_000)
+      expect(result.current).toBeLessThan(givenEnd)
     })
 
-    test("1초가 되지 않았을 때 카운트가 목표 수치 보다 낮아야한다", () => {
-      const { result } = renderHook(() => useCountUp(5_000, 1_000))
+    test.each([320, 9_343, 184_495])(`${seconds}초가 지나도 카운트가 목표 수치(%d)와 같아야 한다`, (givenEnd) => {
+      const { result } = renderHook(() => useCountUp(givenEnd, givenDuration))
 
       act(() => {
-        jest.advanceTimersByTime(500)
+        jest.advanceTimersByTime(givenDuration + 1_000)
       })
 
-      expect(result.current).toBeLessThan(5_000)
-    })
-
-    test("1초가 지나도 카운트가 목표 수치와 같아야 한다", () => {
-      const { result } = renderHook(() => useCountUp(5_000, 1_000))
-
-      act(() => {
-        jest.advanceTimersByTime(1_200)
-      })
-
-      expect(result.current).toBe(5_000)
+      expect(result.current).toBe(givenEnd)
     })
   })
 })
