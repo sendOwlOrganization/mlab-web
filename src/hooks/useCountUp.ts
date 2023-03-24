d89940845ea210a085175851f5d1199b6f49f5a1
Sangbin Lee <leesangbin@outlook.com>
2023-03-25 01:31:04 +0900

    :white_check_mark: useCountUp 구현

--- src/hooks/useCountUp.ts
+++ src/hooks/useCountUp.ts
@@ -1,3 +1,7 @@
+import { useEffect, useState } from "react"
+
+const FRAMERATE = 1_000 / 60
+
 /**
  * 정해진 시간동안 카운트를 0부터 목표까지 증가시키는 훅
  * @param end 목표
@@ -5,7 +9,26 @@
  * @return {number} 카운트
  */
 const useCountUp = (end: number, duration = 1_000) => {
-  return 0
+  const [count, setCount] = useState(0)
+  const totalFrame = Math.round(duration / FRAMERATE)
+
+  useEffect(() => {
+    let currentCount = 0
+
+    const counter = setInterval(() => {
+      const progress = ++currentCount / totalFrame
+      setCount(Math.round(end * progress))
+      if (progress === 1) {
+        clearInterval(counter)
+      }
+    }, FRAMERATE)
+
+    return () => {
+      clearInterval(counter)
+    }
+  }, [end, totalFrame])
+
+  return count
 }
 
 export default useCountUp
