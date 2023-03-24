fd83ca24f3a6b58a7db067ca9adda54690232546
Sangbin Lee <leesangbin@outlook.com>
2023-03-25 01:51:50 +0900

    :sparkles: useCountUp에 가속도 추가

--- src/hooks/useCountUp.ts
+++ src/hooks/useCountUp.ts
@@ -3,6 +3,13 @@ import { useEffect, useState } from "react"
 const FRAMERATE = 1_000 / 60
 
 /**
+ * 가속도 함수
+ * 출처 {@link https://easings.net/ko#easeOutExpo}
+ * @param x
+ */
+const easeOutExpo = (x: number): number => (x === 1 ? 1 : 1 - Math.pow(2, -10 * x))
+
+/**
  * 정해진 시간동안 카운트를 0부터 목표까지 증가시키는 훅
  * @param end 목표
  * @param duration 시간 (ms 단위)
@@ -16,7 +23,7 @@ const useCountUp = (end: number, duration = 1_000) => {
     let currentCount = 0
 
     const counter = setInterval(() => {
-      const progress = ++currentCount / totalFrame
+      const progress = easeOutExpo(++currentCount / totalFrame)
       setCount(Math.round(end * progress))
       if (progress === 1) {
         clearInterval(counter)
