a968c5425764381cbde89453d4677d3c3e219873
Sangbin Lee <leesangbin@outlook.com>
2023-04-01 21:20:50 +0900

    :recycle: custom-instance에 토큰 주입을 interceptor으로 이동

--- src/api/mutator/custom-instance.ts
+++ src/api/mutator/custom-instance.ts
@@ -2,11 +2,26 @@ import { AuthorizationUtil } from "@/utils/AuthorizationUtil";
 import Axios, { AxiosError, AxiosRequestConfig } from "axios";
 
 export const AXIOS_INSTANCE = Axios.create({ baseURL: process.env.NEXT_PUBLIC_API_URL }); // use your own URL here or environment variable
+/**
+ * 로그인 후 access-token이 있으면 저장
+ */
 AXIOS_INSTANCE.interceptors.response.use((response) => {
   if (response.request.responseURL.includes("/api/users/login") && response.headers["access-token"]) {
     AuthorizationUtil.saveToken(response.headers["access-token"]);
   }
   return response;
+});
+/**
+ * Axios config 에 필요시 Authorization 헤더를 추가합니다.
+ */
+AXIOS_INSTANCE.interceptors.request.use((config) => {
+  const accessToken = AuthorizationUtil.getToken();
+
+  if (Boolean(accessToken) && AuthorizationUtil.isAuthorizationRequired(config.url, config.method)) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+
+  return config;
 });
 
 // add a second `options` argument here if you want to pass extra options to each generated query
@@ -14,30 +29,13 @@ export const customInstance = <T>(config: AxiosRequestConfig, options?: AxiosReq
   // eslint-disable-next-line import/no-named-as-default-member
   const source = Axios.CancelToken.source();
 
-  const axiosConfig: AxiosRequestConfig = injectAuthorizationHeader({
+  const axiosConfig: AxiosRequestConfig = {
     ...config,
     ...options,
     cancelToken: source.token
-  });
+  };
 
   return AXIOS_INSTANCE(axiosConfig).then(({ data }) => data);
-};
-
-/**
- * Axios config 에 필요시 Authorization 헤더를 추가합니다.
- * @param config
- */
-const injectAuthorizationHeader = (config: AxiosRequestConfig) => {
-  const accessToken = AuthorizationUtil.getToken();
-
-  if (Boolean(accessToken) && AuthorizationUtil.isAuthorizationRequired(config.url, config.method)) {
-    config.headers = {
-      ...config.headers,
-      Authorization: `Bearer ${accessToken}`
-    };
-  }
-
-  return config;
 };
 
 // In some case with react-query and swr you want to be able to override the return error type so you can also do it here like this
