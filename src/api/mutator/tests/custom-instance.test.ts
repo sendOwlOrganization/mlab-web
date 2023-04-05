d107c4a846a30d57845dda98067d2d12b2cb9b2b
Sangbin Lee <leesangbin@outlook.com>
2023-04-05 20:32:00 +0900

    :bulb: 테스트코드에 주석 추가

--- src/api/mutator/tests/custom-instance.test.ts
+++ src/api/mutator/tests/custom-instance.test.ts
@@ -29,16 +29,22 @@ describe("custom-instance 테스트", () => {
     });
 
     test("토큰을 저장한다", async () => {
+      // arrange
       server.listen();
       const saveTokenSpy = jest.spyOn(AuthorizationUtil, "saveToken");
+
+      // act
       await login({ email: "test", password: "1234" });
 
+      // assert
       expect(saveTokenSpy).toHaveBeenCalledWith("accessTokenForTest");
     });
 
     test("토큰이 필요한 api 헤더에 토큰을 주입해야한다", async () => {
+      // arrange
       server.use(
         rest.get("/api/users/me", (req, res, ctx) => {
+          // assert
           expect(req.headers.get("Authorization")).toBe("Bearer accessTokenForTest");
           return res(ctx.status(200));
         })
@@ -46,11 +52,13 @@ describe("custom-instance 테스트", () => {
       server.listen();
       await login({ email: "test", password: "1234" });
 
+      // act
       await getUserSelf();
     });
 
     describe("토큰이 만료되어 401을 반환했을 때", () => {
       test("토큰을 재발급 받는다", async () => {
+        // arrange
         server.use(
           rest.get("/api/users/me", (req, res, ctx) => {
             const isValidToken = req.headers.get("Authorization") === "Bearer refreshedAccessTokenForTest";
@@ -64,11 +72,13 @@ describe("custom-instance 테스트", () => {
         server.listen();
         await login({ email: "test", password: "1234" });
 
+        // assert
         await expect(getUserSelf()).rejects.toThrow();
         await expect(getUserSelf()).resolves;
       });
 
       test("토큰이 필요하지 않은 api면 재발급 요청을 하지 않는다", async () => {
+        // arrange
         server.use(
           rest.get("/api/balances", (req, res, ctx) => res(ctx.status(200))),
           rest.get("/api/users/refresh", () => {
@@ -78,6 +88,7 @@ describe("custom-instance 테스트", () => {
         server.listen();
         await login({ email: "test", password: "1234" });
 
+        // assert
         await getAllBalance();
       });
     });
