354ea7821251c0ca5d55178ccb5f140b189e3080
Sangbin Lee <leesangbin@outlook.com>
2023-04-09 20:07:13 +0900

    :fire: 프로필 불러오는 이펙트 삭제

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,10 +1,8 @@
 "use client";
 
-import { getUserSelf } from "@/api/generated/hooks";
 import { customInstance } from "@/api/mutator/custom-instance";
-import { useProfile } from "@/store/useProfile";
 import { AuthorizationUtil } from "@/utils/AuthorizationUtil";
-import { ReactNode, useEffect, useState } from "react";
+import { ReactNode, useState } from "react";
 import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { RecoilRoot } from "recoil";
@@ -12,7 +10,6 @@ import { RecoilRoot } from "recoil";
 const CACHE_TIME = 29 * 60 * 1_000;
 
 const LoginProvider = ({ children }: { children: ReactNode }) => {
-  const { profile, updateProfile, logout } = useProfile();
   useQuery({
     // TODO: 임시용, 백엔드 작업 필요
     queryFn: () => customInstance({ method: "get", url: "/api/users/refresh" }),
@@ -21,18 +18,9 @@ const LoginProvider = ({ children }: { children: ReactNode }) => {
     staleTime: CACHE_TIME,
     refetchInterval: CACHE_TIME,
     refetchIntervalInBackground: true,
-    enabled: Boolean(profile) || Boolean(AuthorizationUtil.getToken()),
-    onError: () => logout()
+    enabled: Boolean(AuthorizationUtil.getToken()),
+    onError: () => AuthorizationUtil.saveToken("")
   });
-
-  useEffect(() => {
-    if (AuthorizationUtil.getToken()) {
-      getUserSelf()
-        .then((user) => updateProfile(user))
-        .catch(() => logout());
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
   return <>{children}</>;
 };
